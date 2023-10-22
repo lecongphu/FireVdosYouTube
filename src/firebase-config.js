0fa2e30fca29d753e71ba0134314355edca79217
lecongphu <lecongphu1412@gmail.com>
2023-10-22 10:42:22 +0700

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,11 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyC6_Swy4LW_YkjxB7964NYRneopRncmAjU",
-  authDomain: "j2video.netlify.app",
-  projectId: "schedulesfinder",
-  storageBucket: "schedulesfinder.appspot.com",
-  messagingSenderId: "1047710671835",
-  appId: "1:1047710671835:web:0fa413753343d77522fdfe"
+    authDomain: "schedulesfinder.firebaseapp.com",
+    databaseURL: "https://schedulesfinder.firebaseio.com",
+    projectId: "schedulesfinder",
+    storageBucket: "schedulesfinder.appspot.com",
+    messagingSenderId: "1047710671835",
+    appId: "1:1047710671835:web:bfdcf6311942b2bf22fdfe",
+    measurementId: "G-0WQDNVTRKD"
 };
 
 
